bbca5b45cb62dd7cc2761b7828ec87686a8b87fa
kanix1506 <kanittha_thaotan@cmu.ac.th>
2024-12-20 13:40:20 +0700

    lab10_2 completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,13 +1,3 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-using namespace std;
-
-int main (){
- ifstream source;
- ofstream dest;
- source.open("cheerbook.txt");
- dest.open("cheerbook_copy.txt");
  
  cout<<"-------------------- BOOM ---------------------"<<"\n";
  
